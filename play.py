a315e0d072185f53c8ab319102f3bd5f2556ef34
Petra Kupinski <b.kupinskimn@gmail.com>
2021-09-23 20:24:52 -0400

    player keys tweak

--- play.py
+++ play.py
@@ -24,28 +24,6 @@ vh.autoScaleCenter()
 layout = viewport.ViewLayout([vh])
 
 while True:
-    # determine interact keys available
-    f = pc.pos.facing
-    tempPos = [pc.pos.xy[0], pc.pos.xy[1]]
-    if f == 0:
-        tempPos[1] -= 1
-    elif f == 1:
-        tempPos[0] += 1
-    elif f == 2:
-        tempPos[1] += 1
-    elif f == 3:
-        tempPos[0] -= 1
-
-    obj = None
-    try:
-        obj = theMap.allObjects[tuple(tempPos)]
-    except KeyError:
-        pass
-    if obj != None and obj.interact != None:
-        keys = obj.interact.getKeys(thePos.facing)
-    else:
-        keys = (dict(), dict())
-
     # event handler (player inputs)
     for event in pygame.event.get():
         # if user wants to quit
@@ -54,12 +32,7 @@ while True:
             pygame.quit()
             sys.exit()
         elif event.type == KEYDOWN:
-            if event.key in keys[0]: # interaction keys
-                keys[0][event.key][1]()
-            elif event.key in keys[1]: # hidden interaction keys
-                keys[1][event.key][1]()
-            else:
-                pc.handleKey(event)
+            pc.handleKey(event)
         elif event.type == VIDEORESIZE:
             vh.autoScaleCenter()
 
--- playerchar.py
+++ playerchar.py
@@ -12,7 +12,32 @@ class PlayerCharacter(object):
         self.pos = pos
 
     def handleKey(self, event):
-        if event.key == K_RIGHT: # basic movement keys
+        # determine interact keys available
+        f = self.pos.facing
+        tempPos = [self.pos.xy[0], self.pos.xy[1]]
+        if f == 0:
+            tempPos[1] -= 1
+        elif f == 1:
+            tempPos[0] += 1
+        elif f == 2:
+            tempPos[1] += 1
+        elif f == 3:
+            tempPos[0] -= 1
+        obj = None
+        try:
+            obj = self.pos.map.allObjects[tuple(tempPos)]
+        except KeyError:
+            pass
+        if obj != None and obj.interact != None:
+            keys = obj.interact.getKeys(f.facing)
+        else:
+            keys = (dict(), dict())
+
+        if event.key in keys[0]: # interaction keys
+            keys[0][event.key][1]()
+        elif event.key in keys[1]: # hidden interaction keys
+            keys[1][event.key][1]()
+        elif event.key == K_RIGHT: # basic movement keys
             self.pos.facing += 1
             if self.pos.facing > 3:
                 self.pos.facing -= 4
